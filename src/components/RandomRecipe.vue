cf46556b42414232b92982342f1cd26bbedd5927
kerenKi <kkinberg13@gmail.com>
2019-10-15 19:46:13 +0200

    Styling recipes cards and random button

--- src/components/RandomRecipe.vue
+++ src/components/RandomRecipe.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recipe">
-    <h1>Today's meal:</h1>
+    <a name="top"><h1>Today's meal:</h1></a>
     <h3>{{ recipe.title }}</h3>
     <img :src="recipe.image"/>
     <p> <strong> Category:</strong> {{ recipe.category }}</p>
@@ -11,7 +11,9 @@
       </li>
     </ul>
     <div> <strong> Instruction: </strong>{{ recipe.instructions }}</div>
-    <button @click="onClick"> Show me another random recipe </button>
+    <a href="#top"> 
+      <button @click="onClick"> Show me another random recipe </button>
+    </a>
   </div>
 </template>
 
@@ -87,4 +89,50 @@ export default class RandomRecipe extends Vue {
   ul {
     list-style: none;
   }
+
+/* Reset */
+button {
+  background: transparent;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+  outline: 0;
+  -webkit-appearance: none;
+  margin:35px;
+}
+
+/* Custom */
+button {
+  display: inline-block;
+  position: relative;
+  padding: 20px 38px;
+  top: 0;
+  font-size: 30px;
+  font-family: "Open Sans", Helvetica;
+  border-radius: 4px;
+  border-bottom: 1px solid rgba( 28, 227, 125, 0.5 );
+  background: rgba(66, 103, 90, 0.9);
+  color: #fff;
+  box-shadow: 0px 0px 0px rgba( 15, 165, 60, 0.1 );
+  
+  -webkit-transform: translateZ(0);
+    -moz-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+          transform: translateZ(0);
+  
+  -webkit-transition: all 0.2s ease;
+    -moz-transition: all 0.2s ease;
+      -ms-transition: all 0.2s ease;
+          transition: all 0.2s ease;
+}
+
+button:hover {
+  top: -10px;
+  box-shadow: 0px 10px 10px rgba( 15, 165, 60, 0.2 );
+  
+  -webkit-transform: rotateX(20deg);
+    -moz-transform: rotateX(20deg);
+      -ms-transform: rotateX(20deg);
+          transform: rotateX(20deg);
+}
 </style>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="recipes" >
     <h3>Sort By Category</h3>
-    <select name="category" id="" v-model="selected" @change="getMealsByCategory(selected)">
-      <option value="">Pick a category</option>
-      <option value="beef">Beef</option>
-      <option value="breakfast">Breakfast</option>
-      <option value="chicken">Chicken</option>
-      <option value="dessert">Dessert</option>
-      <option value="goat">Goat</option>
-      <option value="lamb">Lamb</option>
-      <option value="miscellaneous">Miscellaneous</option>
-      <option value="pasta">Pasta</option>
-      <option value="pork">Pork</option>
-      <option value="seafood">Seafood</option>
-      <option value="side">Side</option>
-      <option value="starter">Starter</option>
-      <option value="vegan">Vegan</option>
-      <option value="vegetarian">Vegetarian</option>
-    </select>
-      <ul v-if="selected == ''">
-        <li v-for="option in moreRecipes" :key="option.id">
-          <a @click="goToRecipe(option.id)">
-            {{ option.title }}
-            <img :src="option.image"/>
-          </a>
-        </li>
-      </ul>
+    <div class="custom-select">
+      <select name="category"  v-model="selected" @change="getMealsByCategory(selected)">
+        <option value="">Pick a category</option>
+        <option value="beef">Beef</option>
+        <option value="breakfast">Breakfast</option>
+        <option value="chicken">Chicken</option>
+        <option value="dessert">Dessert</option>
+        <option value="goat">Goat</option>
+        <option value="lamb">Lamb</option>
+        <option value="miscellaneous">Miscellaneous</option>
+        <option value="pasta">Pasta</option>
+        <option value="pork">Pork</option>
+        <option value="seafood">Seafood</option>
+        <option value="side">Side</option>
+        <option value="starter">Starter</option>
+        <option value="vegan">Vegan</option>
+        <option value="vegetarian">Vegetarian</option>
+      </select>
+    </div>  
+      <section v-if="selected == ''" class="cards-container">
+        <div v-for="option in moreRecipes" :key="option.id">
+          <article class="card">
+            <a @click="goToRecipe(option.id)" >
+              <div class="flex-container">
+                <p><img :src="option.image" style="width:100%"/></p>
+                  <h4 class="grid-item">
+                      {{ option.title }}
+                  </h4>
+              </div> 
+            </a>
+          </article>
+        </div>
+      </section>
+
   </div>
 </template>
 
@@ -82,11 +91,5 @@ export default class Recipes extends Vue {
 </script>
 
 <style scoped lang="scss">
-  img {
-    width: 20%;
-  }
 
-  ul {
-    list-style: none;
-  }
 </style>
