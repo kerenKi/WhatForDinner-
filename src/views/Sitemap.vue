e2334e84b6edf647d3afc6cad4d39d5ca83d163e
kerenKi <kkinberg13@gmail.com>
2019-10-28 11:12:48 +0100

    Hard coded sitemap

--- src/views/Sitemap.vue
+++ src/views/Sitemap.vue
@@ -14,26 +14,37 @@ import router from '../router';
 
 @Component
 export default class Sitemap extends Vue {
-  public list: string = '';
+  public list: string[] = [
+    'https://dinner-plans.netlify.com/',
+    'https://dinner-plans.netlify.com/categories',
+    'https://dinner-plans.netlify.com/random',
+    'https://dinner-plans.netlify.com/recipeDetails/:id',
+    'https://dinner-plans.netlify.com/recipes/:category',
+    'https://dinner-plans.netlify.com/sitemap',
+  ];
 
-  public getRoutesList(routes: any, pre: string) {
-    return routes.reduce( (array: string[], route: any) => {
-      const path = `${pre}${route.path}`;
-      if (route.path !== '*') {
-      array.push(path);
-    }
+  // public getRoutesList(routes: any, pre: string) {
+  //   return routes.reduce( (array: string[], route: any) => {
+  //     const path = `${pre}${route.path}`;
+  //     if (route.path !== '*') {
+  //     array.push(path);
+  //   }
 
-      if (route.children) {
-      array.push(...this.getRoutesList(route.children, `${path}/`));
-    }
+  //     if (route.children) {
+  //     array.push(...this.getRoutesList(route.children, `${path}/`));
+  //   }
 
-      return array;
-    }, []);
-  }
+  //     return array;
+  //   }, []);
+  // }
 
-  public mounted() {
-    this.list = this.getRoutesList(router.options.routes, 'https://dinner-plans.netlify.com');
-  }
+  // public mounted() {
+  //   console.log(router)
+  //   // const siteRoutes = this.$router.options.routes
+  //   // console.log(siteRoutes)
+
+  //   this.list = this.getRoutesList(siteRoutes, 'https://dinner-plans.netlify.com');
+  // }
 }
 </script>
 
