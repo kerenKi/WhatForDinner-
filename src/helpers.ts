80b1ca5873d6e3fcedd27d103e1f8b87846768a1
kerenKi <kkinberg13@gmail.com>
2019-10-29 14:34:00 +0100

    Ingredients with measures

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,13 +4,14 @@ export default function constructRecipeFromAPIResponse(meal: any): Recipe {
   /* the structure of the meal API contains 20 keys named strIngredient(1-20).
   In this part i'm parsing through the ingredients and building the full ingredients array */
   const ingredientsArray = [];
-  let endOfIngredient = false;
-  for ( let i = 1; i < 20 && !endOfIngredient; i++) {
+  let endOfIngredients = false;
+  for ( let i = 1; i < 20 && !endOfIngredients; i++) {
     const ingredient = `strIngredient${i}`;
+    const measure = `strMeasure${i}`;
     if (meal[ingredient] !== '') {
-      ingredientsArray.push(meal[ingredient]);
+      ingredientsArray.push(`${meal[measure]} ${meal[ingredient]}`);
     } else {
-      endOfIngredient = true;
+      endOfIngredients = true;
     }
   }
 
